b6d8d2d5701ec33068ebaea178eb215ef365a2ba
Carlos Valdez <chwex@hotmail.com>
2017-09-19 00:14:55 -0600

    register funcionando

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,16 @@
-console.log("hello");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const {sequelize} = require('./models');
+const config = require('./config/config');
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
+
+require('./routes')(app);
 
 app.get('/status', (req, res) => {
     res.send({
@@ -15,10 +18,9 @@ app.get('/status', (req, res) => {
     });
 });
 
-app.post('/register', (req, res) => {
-    res.send({
-       message: `Hello ${req.body.email}, your have been registered!` 
-    });
-});
+sequelize.sync()
+  .then(() => {
 
-app.listen(process.env.PORT || 8081);
+    app.listen(config.port);
+    console.log(`Server started on port ${config.port}`);
+  })
